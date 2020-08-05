02c60efe94b6fb29697a811d109b53522661cd53
Doni Afrizal <doni.afrizal@gmail.com>
2020-08-05 08:08:30 +0700

    2nd commit

---
+++ midtrans_payment/midtrans_payment/doctype/midtrans_payment/midtrans_payment.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, uniklia.com and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Midtrans Payment', {
+	// refresh: function(frm) {
+
+	// }
+});
